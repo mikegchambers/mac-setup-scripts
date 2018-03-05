6916ce7ed38cd219fb9bd08d9166eefa28018f1d
Mike Chambers <mike@Mikes-MBP-2.local>
2018-03-05 22:51:45 +1000

    auto make the terminal pretty

--- settings.sh
+++ settings.sh
@@ -2,6 +2,9 @@
 # sleep settings
 sudo systemsetup -setcomputersleep 30
 # dock settings
+# defaults delete com.apple.dock autohide-delay
 defaults write com.apple.Dock autohide -bool TRUE
-defaults write com.apple.Dock autohide-delay -float 0
 killall Dock
+
+# Get the terminal looking nice...
+cp ./bash_profile ~/.bash_profile
